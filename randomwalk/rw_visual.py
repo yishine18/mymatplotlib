dcb94ccff2c0c636c8e8e81750ee9a9af6170096
hero <hero@126.com>
2020-03-12 16:42:04 +0800

    color start and end .

--- randomwalk/rw_visual.py
+++ randomwalk/rw_visual.py
@@ -7,7 +7,12 @@ while True:
         break
     rw = RandomWalk()
     rw.fill_walk()
-    plt.scatter(rw.x_values,rw.y_values,s = 5)
+    point_numbers = list(range(rw.num_points))
+   # plt.scatter(rw.x_values,rw.y_values,s = 5)
+    plt.scatter(rw.x_values,rw.y_values,c=point_numbers,cmap = plt.cm.Blues,edgecolor='none',s = 15)
+    plt.scatter(0,0,c='green',edgecolor='none',s=100)
+    plt.scatter(rw.x_values[-1],rw.y_values[-1],c='red',edgecolor='none',s=100)
+
     plt.show()
 
 
